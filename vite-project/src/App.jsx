bc9e1e8488900e6c32a074de38feef9789a69e29
Mikaeel92 <mikaeelyaghoobzadeh@gmail.com>
2025-01-08 09:58:16 +0330

    step three useEffect & fetching data

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -8,12 +8,27 @@ const App = () => {
   const [errorMsg, setErrorMsg] = useState(null)
 
   const fetchDataFromApi = async () => {
-      
+      try {
+        setLoading(true)
+        const response = await fetch(`https://api.github.com/users/${input}`)
+        const data = await response.json()
+        if(data && data.length) {
+          setLoading(false)
+          setUserData(data)
+        }
+      } catch (error) {
+        setLoading(false)
+        setErrorMsg(error.message)
+        setInput('')
+      }
   }
 
   useEffect(() => {
     fetchDataFromApi()
   }, [])
+
+  if(loading) {return <div>Data Is Loading Please Wait...</div>}
+  if(errorMsg) {return <div>Error! {errorMsg}</div>}
 
   return (
     <div>App</div>
