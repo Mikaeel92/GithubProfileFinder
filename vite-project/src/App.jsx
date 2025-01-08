18fde2dd8c0400b576c83ecc6913ba2885b0b6dd
Mikaeel92 <mikaeelyaghoobzadeh@gmail.com>
2025-01-08 09:50:56 +0330

    step two states logic

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -1,6 +1,20 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 
 const App = () => {
+
+  const [input, setInput] = useState('')
+  const [userData, setUserData] = useState([])
+  const [loading, setLoading] = useState(false)
+  const [errorMsg, setErrorMsg] = useState(null)
+
+  const fetchDataFromApi = async () => {
+      
+  }
+
+  useEffect(() => {
+    fetchDataFromApi()
+  }, [])
+
   return (
     <div>App</div>
   )
