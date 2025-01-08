9fecddc1921925e0941c34a2944acef7b39b2e43
Mikaeel92 <mikaeelyaghoobzadeh@gmail.com>
2025-01-08 10:05:37 +0330

    step four

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -2,37 +2,54 @@ import React, { useEffect, useState } from 'react'
 
 const App = () => {
 
-  const [input, setInput] = useState('')
   const [userData, setUserData] = useState([])
+  const [input, setInput] = useState('hoomantalakian')
   const [loading, setLoading] = useState(false)
   const [errorMsg, setErrorMsg] = useState(null)
 
-  const fetchDataFromApi = async () => {
+  const handleSubmit = () => {
+      fetchApi() 
+  }
+  const fetchApi = async () => {
       try {
-        setLoading(true)
-        const response = await fetch(`https://api.github.com/users/${input}`)
-        const data = await response.json()
-        if(data && data.length) {
+          setLoading(true)
+          const res = await fetch(`https://api.github.com/users/${input}`);
+          const data = await res.json()
+          console.log(data)
+          if(data) {
+              setUserData(data)
+              setLoading(false)
+              setInput('')
+          }
+      } 
+      catch (e) {
+          setErrorMsg(e.message)
           setLoading(false)
-          setUserData(data)
-        }
-      } catch (error) {
-        setLoading(false)
-        setErrorMsg(error.message)
-        setInput('')
       }
   }
-
   useEffect(() => {
-    fetchDataFromApi()
+      fetchApi()
   }, [])
 
   if(loading) {return <div>Data Is Loading Please Wait...</div>}
   if(errorMsg) {return <div>Error! {errorMsg}</div>}
 
-  return (
-    <div>App</div>
-  )
-}
+return (
+  <div className='w-[700px] h-[750px] mx-auto mt-10 border border-gray-400 p-4 flex flex-col items-center justify-center shadow-lg rounded-lg'>
+      <div className='flex gap-2 items-center justify-center my-10'>
+      <input
+      className='px-4 py-2 bg-gray-100 rounded-md font-bold flex items-center justify-center'
+      value={input} 
+      onChange={(e) => setInput(e.target.value) }
+      type='text'
+      placeholder='Search Github UserName...' 
+      />
+      <button onClick={handleSubmit} className='bg-blue-600 rounded-md p-2 hover:bg-blue-800 text-white'>Search</button>
+      </div>
+      {
+          userData ? <Card user={userData}/> : null
+      }
+  </div>
+)}
 
 export default App
---
+++ vite-project/src/Card.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Card = ({user}) => {
+
+    const {avatar_url, followers, following, public_repos, url, name, login, created_at} = user
+
+  return (
+    <div>Card</div>
+  )
+}
+
+export default Card
